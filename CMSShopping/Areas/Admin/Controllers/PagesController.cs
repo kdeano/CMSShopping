ceb1ccdfe97ee465a0fe927b2b43fe8a8c2c87c3
KYLE DEAN <b9015015@hallam.shu.ac.uk>
2022-04-07 09:19:21 +0100

    Admin page view

---
+++ CMSShopping/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,31 @@
+﻿using CMSShopping.Infrastructure;
+using CMSShopping.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSShopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {
+        private readonly CmsShoppingContext context;
+
+        public PagesController(CmsShoppingContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+
+            List<Page> pagesList = await pages.ToListAsync();
+
+            return View(pagesList);
+        }
+    }
+}
