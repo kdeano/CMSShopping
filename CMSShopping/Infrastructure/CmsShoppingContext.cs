00ec5ddcdfad02cd23fd70d5eac1d2567f53dbcc
KYLE DEAN <b9015015@hallam.shu.ac.uk>
2022-04-07 08:30:23 +0100

    db

---
+++ CMSShopping/Infrastructure/CmsShoppingContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSShopping.Infrastructure
+{
+    public class CmsShoppingContext : DbContext
+    {
+        public CmsShoppingContext(DbContextOptions<CmsShoppingContext> options) : base(options)
+        {
+
+        }
+    }
+}
